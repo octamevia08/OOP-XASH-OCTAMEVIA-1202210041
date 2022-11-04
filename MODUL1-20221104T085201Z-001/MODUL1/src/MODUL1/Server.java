ce8b7b691af73da722793ff23bfc54dfb1282696
octamevia08 <103402352+octamevia08@users.noreply.github.com>
2022-11-04 16:47:32 +0700

    Add files via upload

---
+++ MODUL1-20221104T085201Z-001/MODUL1/src/MODUL1/Server.java
@@ -0,0 +1,30 @@
+package MODUL1;
+
+import java.util.Scanner;
+
+public class Server {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Selamat Datang di Restoran EAD");
+        System.out.println("Silahkan Registrasi Terlebih Dahulu");
+        System.out.println("=======================================");
+        System.out.println("Nama : " +(User.nama));
+        System.out.println("No. Handphone : " +(User.nomor));
+
+
+        System.out.println("======================================");
+        System.out.println("1. Menu\n2. Pilih Menu\n3. Keluar");
+        System.out.println("======================================");
+        System.out.println("Masukkan Pilihan : ");
+
+        // TODO Insert Menu to Database
+
+
+        // TODO Display Main Menu
+
+        // TODO Create User
+
+        // Display Menu
+    }
+}
---
+++ MODUL1-20221104T085201Z-001/MODUL1/src/MODUL1/User.java
@@ -0,0 +1,15 @@
+package MODUL1;
+import java.util.Scanner;
+
+        public class User {
+            public static int nama = (User.nama);
+            public static int nomor = (User.nomor);
+            
+            protected static int getUser() {
+                return nama;
+            }
+        
+            User() {
+                nama++;
+            }
+        }
